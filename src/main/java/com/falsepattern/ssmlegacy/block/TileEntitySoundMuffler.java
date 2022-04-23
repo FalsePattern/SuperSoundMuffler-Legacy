4f1f7de19f9a9ea0752cb47c4da63b791c6ed6de
FalsePattern <30945458+falsepattern@users.noreply.github.com>
2022-04-23 17:33:47 +0200

    port TileEntitySoundMuffler

--- src/main/java/com/falsepattern/ssmlegacy/block/TileEntitySoundMuffler.java
+++ src/main/java/com/falsepattern/ssmlegacy/block/TileEntitySoundMuffler.java
@@ -1,24 +1,25 @@
 package com.falsepattern.ssmlegacy.block;
 
 import com.falsepattern.ssmlegacy.SuperSoundMuffler;
-import net.minecraft.block.state.IBlockState;
+import lombok.val;
+import net.minecraft.block.Block;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TileEntitySoundMuffler extends TileEntity {
-    private HashSet<ResourceLocation> muffledSounds = new HashSet<>();
+    private final Set<ResourceLocation> muffledSounds = new HashSet<>();
     private boolean whiteListMode = true;
     private static final int[] ranges = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 32, 64, 128, 256 };
     private static final int defaultRangeIndex = 7;
@@ -38,14 +39,12 @@ public class TileEntitySoundMuffler extends TileEntity {
 
     //region NBT Serialization
     @Override
-    @Nonnull
-    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+    public void writeToNBT(NBTTagCompound compound) {
         super.writeToNBT(compound);
-        compound = writeNBT(compound);
-        return compound;
+        writeNBT(compound);
     }
 
-    public NBTTagCompound writeNBT(NBTTagCompound compound) {
+    public void writeNBT(NBTTagCompound compound) {
         NBTTagList tagList = new NBTTagList();
         for (ResourceLocation sound : muffledSounds) {
             NBTTagCompound tag = new NBTTagCompound();
@@ -55,8 +54,6 @@ public class TileEntitySoundMuffler extends TileEntity {
         compound.setTag("sounds", tagList);
         compound.setBoolean("whiteList", whiteListMode);
         compound.setFloat("rangeIndex", rangeIndex);
-
-        return compound;
     }
 
     @Override
@@ -80,27 +77,20 @@ public class TileEntitySoundMuffler extends TileEntity {
     }
 
     @Override
-    @Nonnull
-    public NBTTagCompound getUpdateTag() {
-        NBTTagCompound compound = super.getUpdateTag();
-        compound = writeNBT(compound);
-        return compound;
+    public Packet getDescriptionPacket() {
+        val compound = new NBTTagCompound();
+        writeToNBT(compound);
+        return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 0, compound);
     }
 
     @Override
-    public SPacketUpdateTileEntity getUpdatePacket() {
-        NBTTagCompound compound = getUpdateTag();
-        return new SPacketUpdateTileEntity(pos, 0, compound);
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {
+        readFromNBT(pkt.func_148857_g());
     }
 
     @Override
-    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
-        readFromNBT(packet.getNbtCompound());
-    }
-
-    @Override
-    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
-        return oldState.getBlock() != newSate.getBlock();
+    public boolean shouldRefresh(Block oldBlock, Block newBlock, int oldMeta, int newMeta, World world, int x, int y, int z) {
+        return oldBlock != newBlock;
     }
 
     boolean isDefault() {
@@ -112,16 +102,12 @@ public class TileEntitySoundMuffler extends TileEntity {
         muffledSounds.add(sound);
 
         markDirty();
-        IBlockState state = world.getBlockState(pos);
-        world.notifyBlockUpdate(pos, state, state, 4);
     }
 
     public void unmuffleSound(ResourceLocation sound) {
         muffledSounds.remove(sound);
 
         markDirty();
-        IBlockState state = world.getBlockState(pos);
-        world.notifyBlockUpdate(pos, state, state, 4);
     }
 
     public List<ResourceLocation> getMuffledSounds() {
@@ -132,8 +118,6 @@ public class TileEntitySoundMuffler extends TileEntity {
         whiteListMode = !whiteListMode;
 
         markDirty();
-        IBlockState state = world.getBlockState(pos);
-        world.notifyBlockUpdate(pos, state, state, 4);
     }
 
     public boolean isWhiteList() { return whiteListMode; }
@@ -147,17 +131,15 @@ public class TileEntitySoundMuffler extends TileEntity {
     }
 
     public boolean shouldMuffleSound(ISound sound) {
-        double dist = getDistanceSq(sound.getXPosF(), sound.getYPosF(), sound.getZPosF());
+        double dist = getDistanceFrom(sound.getXPosF(), sound.getYPosF(), sound.getZPosF());
         int range = getRange();
-        return dist <= ((range * range) + 1) && shouldMuffleSound(sound.getSoundLocation());
+        return dist <= ((range * range) + 1) && shouldMuffleSound(sound.getPositionedSoundLocation());
     }
 
     public void setRange(int value) {
         rangeIndex = value;
 
         markDirty();
-        IBlockState state = world.getBlockState(pos);
-        world.notifyBlockUpdate(pos, state, state, 4);
     }
 
     public int getRange() {
