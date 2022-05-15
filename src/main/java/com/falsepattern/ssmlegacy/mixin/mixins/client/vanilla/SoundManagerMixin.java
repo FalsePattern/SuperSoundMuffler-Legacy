e2e6c97c4e56850d0420ffab5e14af6d5cbcb8d9
FalsePattern <30945458+falsepattern@users.noreply.github.com>
2022-05-15 13:52:53 +0200

    temporary workaround to fix a crash

--- src/main/java/com/falsepattern/ssmlegacy/mixin/mixins/client/vanilla/SoundManagerMixin.java
+++ src/main/java/com/falsepattern/ssmlegacy/mixin/mixins/client/vanilla/SoundManagerMixin.java
@@ -6,7 +6,11 @@ import net.minecraft.client.audio.SoundManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 
 @Mixin(SoundManager.class)
 public abstract class SoundManagerMixin {
@@ -17,6 +21,20 @@ public abstract class SoundManagerMixin {
     private void handleCancel(ISound sound, CallbackInfo ci) {
         if (SuperSoundMuffler.instance != null && SuperSoundMuffler.instance.shouldMuffle(sound)) {
             ci.cancel();
+        }
+    }
+
+    // Temporary workaround for https://github.com/FalsePattern/SuperSoundMuffler-Legacy/issues/2
+    @Redirect(method = "updateAllSounds",
+              at = @At(value = "INVOKE",
+                       target = "Ljava/util/Iterator;hasNext()Z"))
+    private boolean tryFixFallback(Iterator instance) {
+        try {
+            return instance.hasNext();
+        } catch (ConcurrentModificationException e) {
+            e.printStackTrace();
+            SuperSoundMuffler.log.warn("SSM suppressed a crash in the sound manager. This is a temporary fix until the true reason is found.");
+            return false;
         }
     }
 }
